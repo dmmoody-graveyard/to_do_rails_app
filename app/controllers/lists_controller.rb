b709ba17648b72ab02c154980494ad090fe2749a
Duane M. Moody <dmmoody@gmail.com>
2015-03-16 11:20:53 -0700

    add ability to create another list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,18 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render:new
+    end
+  end
 end
